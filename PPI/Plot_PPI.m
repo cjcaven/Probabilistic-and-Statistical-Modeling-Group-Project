ce9d68e54764243c1a011d21aeaa0d41b23780d9
Lisa Paulsen <72503842+wortsatz@users.noreply.github.com>
2023-08-15 10:47:44 +0200

    update on figures and support scripts

--- PPI/Plot_PPI.m
+++ PPI/Plot_PPI.m
@@ -4,105 +4,74 @@
 
 % Create Data Paths
 
-%sub1
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-001/PPI/PPI-Stim-Img/PPI_1-right_stim-img.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-001/PPI/PPI-Stim-Img/PPI_OP4-right_Stim-Img.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-001/PPI/PPI-Stim-Img/PPI_3b-right_Stim-Img.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-001/PPI/PPI-Img-Stim/PPI_1-right_Img-Stim.mat') ;
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-001/PPI/PPI-Img-Stim/PPI_OP4-right_Img-Stim.mat');
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-001/PPI/PPI-Img-Stim/PPI_3b-right_Img-Stim.mat');
-%sub2
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-002/PPI/PPI_1-right_stim-img_sub002.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-002/PPI/PPI_3b-right_stim-img_sub002.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-002/PPI/PPI_1-right_img-stim_sub002.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-002/PPI/PPI_3b-right_img-stim_sub002.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-002/PPI/PPI_OP4-right_Img-Stim.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-002/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub3
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-003/PPI/PPI_1-right_stim-img_sub003.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-003/PPI/PPI_3b-right_stim-img_sub003.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-003/PPI/PPI_1-right_img-stim_sub003.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-003/PPI/PPI_3b-right_img-stim_sub003.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-003/PPI/PPI_OP4-right_Img-Stim_sub003.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-003/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub4
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-004/PPI/PPI_1-right_stim-img_sub004.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-004/PPI/PPI_3b-right_stim-img_sub004.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-004/PPI/PPI_1-right_img-stim_sub004.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-004/PPI/PPI_3b-right_img-stim_sub004.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-004/PPI/PPI_OP4-right_Img-Stim_sub004.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-004/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub5
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-005/PPI/PPI_1-right_stim-img_sub005.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-005/PPI/PPI_3b-right_stim-img_sub005.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-005/PPI/PPI_1-right_img-stim_sub005.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-005/PPI/PPI_3b-right_img-stim_sub005.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-005/PPI/PPI_OP4-right_Img-Stim_sub005.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-005/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub6
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-006/PPI/PPI_1-right_stim-img_sub006.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-006/PPI/PPI_3b-right_stim-img_sub006.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-006/PPI/PPI_1-right_img-stim_sub006.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-006/PPI/PPI_3b-right_img-stim_sub006.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-006/PPI/PPI_OP4-right_Img-Stim_sub006.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-006/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub7
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-007/PPI/PPI_1-right_stim-img_sub007.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-007/PPI/PPI_3b-right_stim-img_sub007.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-007/PPI/PPI_1-right_img-stim_sub007.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-007/PPI/PPI_3b-right_img-stim_sub007.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-007/PPI/PPI_OP4-right_Img-Stim.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-007/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub8
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-008/PPI/PPI_1-right_stim-img_sub008.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-008/PPI/PPI_3b-right_stim-img_sub008.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-008/PPI/PPI_1-right_img-stim_sub008.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-008/PPI/PPI_3b-right_img-stim_sub008.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-008/PPI/PPI_OP4-right_Img-Stim.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-008/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub9
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-009/PPI/PPI_1-right_stim-img_sub009.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-009/PPI/PPI_3b-right_stim-img_sub009.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-009/PPI/PPI_1-right_img-stim_sub009.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-009/PPI/PPI_3b-right_img-stim_sub009.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-009/PPI/PPI_OP4-right_Img-Stim.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-009/PPI/PPI_OP4-right_Stim-Img.mat');
-%sub10
-stim_img_one_r = load('/Volumes/LP2/Stats23_data/sub-010/PPI/PPI_1-right_stim-img_sub010.mat');
-stim_img_threeb_r = load('/Volumes/LP2/Stats23_data/sub-010/PPI/PPI_3b-right_stim-img_sub010.mat');
-img_stim_one_r = load('/Volumes/LP2/Stats23_data/sub-010/PPI/PPI_1-right_img-stim_sub010.mat') ;
-img_stim_threeb_r = load('/Volumes/LP2/Stats23_data/sub-010/PPI/PPI_3b-right_img-stim_sub010.mat');
-stim_img_op4_r = load('/Volumes/LP2/Stats23_data/sub-010/PPI/PPI_OP4-right_Img-Stim.mat');
-img_stim_op4_r = load('/Volumes/LP2/Stats23_data/sub-010/PPI/PPI_OP4-right_Stim-Img.mat');
+% Define the list of participants
+participant_numbers = {'001', '002', '003', '004', '005', '006', '007', '008', '009', '010'};
 
-% Plot Figure
+% Define the cobinations and combination titles for the figure
+pairs = {'1r', '3br'; '1r', 'op4r'; '3br', 'op4r'};
+pair_names = {'1', '3b'; '1', 'OP4'; '3b', 'OP4'};
 
-figure
-plot(img_stim_threeb_r.PPI.ppi,img_stim_op4_r.PPI.ppi,'k.');
-hold on
-plot(stim_img_threeb_r.PPI.ppi,stim_img_op4_r.PPI.ppi,'r.');
+% iteratre over all particpiants to create the figure
+for p = 1:length(participant_numbers)
+    participant_num = participant_numbers{p};
+    
+    % Load data for the current participant
+    for pair_idx = 1:size(pairs, 1)
+        img1 = sprintf('img%s', pairs{pair_idx, 1});
+        img2 = sprintf('img%s', pairs{pair_idx, 2});
+        stim1 = sprintf('stim%s', pairs{pair_idx, 1});
+        stim2 = sprintf('stim%s', pairs{pair_idx, 2});
+        
+        %load the data for stim and img PPI mat files for 2 of the
+        %subregions
+        data.(img1) = load(sprintf('/Volumes/LP2/Stats23_data/sub-%s/PPI/PPI_%s-img.mat', participant_num, pairs{pair_idx, 1}));
+        data.(img2) = load(sprintf('/Volumes/LP2/Stats23_data/sub-%s/PPI/PPI_%s-img.mat', participant_num, pairs{pair_idx, 2}));
+        data.(stim1) = load(sprintf('/Volumes/LP2/Stats23_data/sub-%s/PPI/PPI_%s-stim.mat', participant_num, pairs{pair_idx, 1}));
+        data.(stim2) = load(sprintf('/Volumes/LP2/Stats23_data/sub-%s/PPI/PPI_%s-stim.mat', participant_num, pairs{pair_idx, 2}));
+    end
+    
+    % Iterate through pairs and generate plots
+    for pair_idx = 1:size(pairs, 1)
+        img1 = sprintf('img%s', pairs{pair_idx, 1});
+        img2 = sprintf('img%s', pairs{pair_idx, 2});
+        stim1 = sprintf('stim%s', pairs{pair_idx, 1});
+        stim2 = sprintf('stim%s', pairs{pair_idx, 2});
+        
+        % initialize the figure
+        figure;
+        plot(data.(img1).PPI.ppi, data.(img2).PPI.ppi, 'k.');
+        hold on
+        plot(data.(stim1).PPI.ppi, data.(stim2).PPI.ppi,'r.');
+        
+        % x and y axis has always the range of -1.5 to 1.5
+        xlim([-1.5, 1.5]);
+        ylim([-1.5, 1.5]);
 
-% Plot the best fit lines for Img>Stim PPI
-x = img_stim_threeb_r.PPI.ppi(:);
-x = [x, ones(size(x))];
-y = img_stim_op4_r.PPI.ppi(:);
-B = x\y;
-y1 = B(1)*x(:,1)+B(2);
-plot(x(:,1),y1,'k-');
+        % Plot the best fit line for the PPI of Imagery
+        x = data.(img1).PPI.ppi(:);
+        x = [x, ones(size(x))];
+        y = data.(img2).PPI.ppi(:);
+        B = x\y;
+        y1 = B(1)*x(:,1)+B(2);
+        plot(x(:,1), y1, 'k-');
 
-% Plot the best fit line for Stim>Img PPI
-x = stim_img_threeb_r.PPI.ppi(:);
-x = [x, ones(size(x))];
-y = stim_img_op4_r.PPI.ppi(:);
-B = x\y;
-y1 = B(1)*x(:,1)+B(2);
-plot(x(:,1),y1,'r-');
+        % Plot the best fit lin e for the PPI of Stimulus
+        x = data.(stim1).PPI.ppi(:);
+        x = [x, ones(size(x))];
+        y = data.(stim2).PPI.ppi(:);
+        B = x\y;
+        y1 = B(1)*x(:,1)+B(2);
+        plot(x(:,1), y1, 'r-');
 
-% Create legend and labels
-legend('Imaginary','Stimulus')
-xlabel('S1 activity')
-ylabel('S2 activity')
-title('Subject 1')
+        % Create legend and labels
+        legend('Imaginary', 'Stimulus');
+        xlabel(sprintf('Area %s activity', pair_names{pair_idx, 1}));
+        ylabel(sprintf('Area %s activity', pair_names{pair_idx, 2}));
+        title(['Subject ', participant_num]);
 
-saveas(gcf, '/Users/lp1/Documents/GitHub/Probabilistic-and-Statistical-Modeling-Group-Project/PPI/Figures/figure_sub001.png');
+        % Save the figure
+        figure_filename = sprintf('/Users/lp1/Documents/GitHub/Probabilistic-and-Statistical-Modeling-Group-Project/PPI/Figures/figure_sub%s_pair%s-%s.png', participant_num, pair_names{pair_idx, 1}, pair_names{pair_idx, 2});
+        saveas(gcf, figure_filename);
+    end
+end
 
