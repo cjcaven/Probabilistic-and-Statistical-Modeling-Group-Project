0407ed5f8f8978025fad52c4ff626837c921ad31
magdamalinowska29 <73298429+magdamalinowska29@users.noreply.github.com>
2023-08-20 22:27:05 +0200

    calculate mean correlation per cluster

---
+++ RSA_Analysis/correlation_per_cluster.m
@@ -0,0 +1,18 @@
+%this script calculates the avg result for a cluster, you need to
+%provide the path to the cluster mask and the image with the coreelation
+%coefficients averaged over participans
+
+cluster_file= 'C:\shared_data\RSA_results\type\cluster1_mask.nii';
+cluster_mask=spm_read_vols(spm_vol(cluster_file));
+
+
+image='C:\shared_data\RSA_results\type\avg_correlation_2.nii';
+hdr=spm_vol(image);
+vol= spm_read_vols(hdr, cluster_mask);
+
+values=cluster_mask>0;
+
+
+cluster_acc=mean(vol(values),[1 2 3],'double','omitnan');
+
+
