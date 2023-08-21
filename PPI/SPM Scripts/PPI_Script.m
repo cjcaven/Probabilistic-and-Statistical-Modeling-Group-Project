0f286acaecf65e6fe84b30f5dff99292d60151a8
Lisa Paulsen <72503842+wortsatz@users.noreply.github.com>
2023-08-21 22:23:06 +0200

    update to folder structures

--- PPI/SPM Scripts/PPI_Script.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/Users/lp1/Documents/GitHub/Probabilistic-and-Statistical-Modeling-Group-Project/PPI/SPM Scripts/PPI_Script_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
