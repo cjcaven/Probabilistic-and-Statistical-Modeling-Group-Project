0e49c939d3dfa56d49c9a5a5d100a66cd56fc587
magdamalinowska29 <73298429+magdamalinowska29@users.noreply.github.com>
2023-08-21 19:03:59 +0200

    added sd

--- RSA_Analysis/correlation_per_cluster.m
+++ RSA_Analysis/correlation_per_cluster.m
@@ -2,17 +2,23 @@
 %provide the path to the cluster mask and the image with the coreelation
 %coefficients averaged over participans
 
-cluster_file= 'C:\shared_data\RSA_results\type\cluster1_mask.nii';
+cluster_file= 'C:\shared_data\RSA_results\type\cluster5_mask.nii'; %specify your binary cluster mask
 cluster_mask=spm_read_vols(spm_vol(cluster_file));
 
 
-image='C:\shared_data\RSA_results\type\avg_correlation_2.nii';
+image='C:\shared_data\RSA_results\type\avg_correlation_2.nii'; %specify your correlaiton map, averaged over participants
 hdr=spm_vol(image);
 vol= spm_read_vols(hdr, cluster_mask);
 
-values=cluster_mask>0;
+values=cluster_mask>0; %get the indicies of the mask voxels
 
 
-cluster_acc=mean(vol(values),'double','omitnan');
+cluster_mean=mean(vol(values),'double','omitnan'); %calculate mean correlation
+
+cluster_sd=std(vol(values), 1,'all'); %calculate standard deviation
+
+
+
+
 
 
